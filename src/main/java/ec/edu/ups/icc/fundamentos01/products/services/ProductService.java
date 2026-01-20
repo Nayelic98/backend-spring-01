d0883a9f6f8dc148b327b18e035bc34f4f39d3b0
Nayelic98 <cajamarcanaye@gmail.com>
2026-01-20 17:53:02 -0500

    Primer commit - Proyecto

---
+++ src/main/java/ec/edu/ups/icc/fundamentos01/products/services/ProductService.java
@@ -0,0 +1,39 @@
+package ec.edu.ups.icc.fundamentos01.products.services;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Slice;
+
+import ec.edu.ups.icc.fundamentos01.products.dtos.CreateProductDto;
+
+import ec.edu.ups.icc.fundamentos01.products.dtos.UpdateProductDto;
+import ec.edu.ups.icc.fundamentos01.products.dtos.ProductResponseDto;
+
+public interface ProductService {
+
+    ProductResponseDto create(CreateProductDto dto);
+
+    Page<ProductResponseDto> findAllPaginado(int page, int size, String[] sort);
+    List<ProductResponseDto> findAll();
+
+    ProductResponseDto findById(Long id);
+
+    List<ProductResponseDto> findByUserId(Long id);
+
+    List<ProductResponseDto> findByCategoryId(Long id);
+
+    ProductResponseDto update(Long id, UpdateProductDto dto);
+    Slice<ProductResponseDto> findAllSlice(int page, int size, String[] sort);
+    Page<ProductResponseDto> findWithFilters(
+        String name, 
+        Double minPrice, 
+        Double maxPrice, 
+        Long categoryId,
+        int page, 
+        int size, 
+        String[] sort
+    );
+    void delete(Long id);
+
+}
